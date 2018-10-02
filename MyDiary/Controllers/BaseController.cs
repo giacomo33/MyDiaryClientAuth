d5b3a34aa3846b88562bbde857a7ff1ce69f5205
Giacomo Mirabile <gm@eworkspace.com.au>
2018-10-02 18:53:06 +1000

    added

---
+++ MyDiary/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Mobile.Server;
+using Microsoft.Azure.Mobile.Server.Tables;
+using MyDiary.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+using System.Web.Http.Controllers;
+
+namespace MyDiary.Controllers
+{
+    //This is an abstract class so we can apply inheritance to scalfolded tablecontrollers<T>.
+    public abstract class BaseController<TModel> : TableController<TModel> where TModel : class, ITableData
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            MobileServiceContext context = new MobileServiceContext();
+            SetDomainManager(new EntityDomainManager<TModel>(context, Request));
+        }
+
+        public void SetDomainManager(EntityDomainManager<TModel> domainManager)
+        {
+            DomainManager = domainManager;
+        }
+    }
+}
